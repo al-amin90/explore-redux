5bb7142cc9f1081f012392e14e56071a42b4c2a0
al-amin90 <ijesun30@gmail.com>
2025-04-09 16:47:33 +0600

    react redux setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Counter with redux</h1>
+
+      <button>Increment</button>
+      <p>0</p>
+      <button>Decrement</button>
+    </div>
+  )
+}
+
+export default App
